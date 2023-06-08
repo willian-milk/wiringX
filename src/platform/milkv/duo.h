7531e6e8d7bf0e5b41acd6fcb2dacb2983d0e3de
willian-milk <willian@milkv.io>
2023-06-08 18:26:08 +0800

    add milkv duo platform
    Signed-off-by:  willian-milk <willian@milkv.io>

---
+++ src/platform/milkv/duo.h
@@ -0,0 +1,19 @@
+/*
+  Copyright (c) 2023  MilkV Ltd.
+  Author: Willian <willian@milkv.io>
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef __WIRINGX_DUO_H_
+#define __WIRINGX_DUO_H_
+
+#include "../platform.h"
+
+extern struct platform_t *duo;
+
+void milkv_duoInit(void);
+
+#endif

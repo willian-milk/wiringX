4ba3f7bdab46fa76c842a11544a0478095bbfdbe
willian-milk <willian@milkv.io>
2023-06-08 18:39:06 +0800

    add python support for milk-v duo on sophgo
    Signed-off-by:  willian-milk <willian@milkv.io>

--- python/wiringX/wiringx.c
+++ python/wiringX/wiringx.c
@@ -409,6 +409,7 @@ PyMODINIT_FUNC initgpio(void) {
 	PyModule_AddObject(module, "HUMMINGBOARD_GATE_DQ", Py_BuildValue("s", "hummingboard_gate_dq"));
 	PyModule_AddObject(module, "HUMMINGBOARD_GATE_SDL", Py_BuildValue("s", "hummingboard_gate_sdl"));
 	PyModule_AddObject(module, "ROCK4", Py_BuildValue("s", "rock4"));
+	PyModule_AddObject(module, "DUO", Py_BuildValue("s", "duo"));
 
 #if PY_MAJOR_VERSION >= 3
 	return module;
